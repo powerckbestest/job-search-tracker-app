195f13bb69e1c8f8780562a3764306025676282a
Anton.Valkovskiy <av@item.expert>
2024-11-15 20:21:04 +0100

    refactoring/#9-add-state-manager добавлен слайс для хранения текущих активных значений

--- src/model/store.ts
+++ src/model/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import {employersSlice} from "./employers.ts";
+import {useDispatch, useSelector} from "react-redux";
+import {valuesSlice} from "./values.ts";
 
 export const store = configureStore({
     reducer: {
+        [valuesSlice.name]: valuesSlice.reducer,
         [employersSlice.name]: employersSlice.reducer,
     },
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>() //
+export const useAppSelector = useSelector.withTypes<RootState>() //
---
+++ src/model/values.ts
@@ -0,0 +1,33 @@
+import {createSelector, createSlice} from "@reduxjs/toolkit";
+import {RootState} from "./store.ts";
+
+
+type Values = {
+    editingEmployerId: string | null;
+    isAdding: boolean;
+}
+
+const initialValues: Values = {
+    editingEmployerId: null,
+    isAdding: false,
+}
+
+export const valuesSlice = createSlice({
+    name: 'values',
+    initialState: initialValues,
+    reducers: {
+        setEditingEmployerId: (state, action) => {
+            state.editingEmployerId = action.payload;
+        },
+        resetEditingEmployerId: (state) => {
+            state.editingEmployerId = null;
+        },
+        setIsAdding: (state, action) => {
+            state.isAdding = action.payload;
+        },
+    },
+})
+
+const valuesState = (state:RootState):Values => state.values;
+export const selectValueIsAdding = createSelector(valuesState,(s)=>s.isAdding);
+export const selectValueEditingEmployerId = createSelector(valuesState,(s)=>s.editingEmployerId || "");
