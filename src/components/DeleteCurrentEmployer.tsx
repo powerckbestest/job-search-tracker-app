82ed8d10a52e9d36b6259e4a465d8037ed76efa6
Anton.Valkovskiy <av@item.expert>
2024-11-24 16:11:08 +0100

    refactoring/#15-shad-cn-integration обновлен диалог на удаление

--- src/components/DeleteCurrentEmployer.tsx
+++ src/components/DeleteCurrentEmployer.tsx
@@ -1,9 +1,23 @@
 import { Employer } from '../types.ts';
-import  { FC, useState } from 'react';
-import { useAppDispatch } from '../model/store.ts';
-import { employersSlice } from '../model/employers.ts';
+import { FC, useState } from 'react';
+import { RootState, useAppDispatch } from '../model/store.ts';
+import { employersSelectors, employersSlice } from '../model/employers.ts';
 import { Trash2, Ban } from 'lucide-react';
 import { Button } from '@/components/ui/button.tsx';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { useSelector } from 'react-redux';
+
 type DeleteCurrentEmployerProps = {
   employerId: Employer['id'];
 };
@@ -11,52 +25,55 @@ export const DeleteCurrentEmployer: FC<DeleteCurrentEmployerProps> = ({
   employerId,
 }) => {
   const dispatch = useAppDispatch();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [, setIsOpen] = useState<boolean>(false);
+  const employer = useSelector((state: RootState) =>
+    employersSelectors.selectById(state, employerId)
+  );
 
   return (
-    <>
-      <Button
-        variant="ghost"
-        data-testid="deleteEmployer"
-        onClick={() => {
-          setIsOpen(true);
-        }}
-        className="rounded-md px-3 py-1 text-sm text-red-600 transition-colors hover:bg-red-50"
-      >
-        <Trash2 />
-      </Button>
-      <dialog
-        className="rounded-lg border-2 border-gray-200 bg-white shadow-lg"
-        open={isOpen}
-      >
-        <div className="p-4 text-center">
-          <h3 className="text-lg font-medium leading-6 text-gray-900">
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button
+          variant="ghost"
+          data-testid="deleteEmployer"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+          className="rounded-md px-3 py-1 text-sm text-red-600 transition-colors hover:bg-red-50"
+        >
+          <Trash2 />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="rounded-lg border-2 border-gray-200 bg-white shadow-lg">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Удаление {employer?.companyName}</AlertDialogTitle>
+          <AlertDialogDescription>
             Вы уверены, что хотите удалить этого работодателя?
-          </h3>
-          <div className="direction-row mt-2 flex items-center justify-between gap-2">
-            <Button
-              data-testid="confirm-cancel"
-              onClick={() => {
-                setIsOpen(false);
-              }}
-              className="direction-row flex items-center gap-2 rounded-md bg-blue-500 px-3 py-2 text-sm text-white transition-colors hover:bg-blue-700"
-            >
-              <Ban />
-              Отмена
-            </Button>
-            <Button
-              data-testid="confirm-delete"
-              onClick={() => {
-                dispatch(employersSlice.actions.deleteEmployer(employerId));
-                setIsOpen(false);
-              }}
-              className="direction-row flex items-center gap-2 rounded-md bg-red-500 px-3 py-2 text-sm text-white transition-colors hover:bg-red-700"
-            >
-              <Trash2 /> Удалить
-            </Button>
-          </div>
-        </div>
-      </dialog>
-    </>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            data-testid="confirm-cancel"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            className="direction-row flex items-center gap-2 rounded-md bg-blue-500 px-3 py-2 text-sm text-white transition-colors hover:bg-blue-700 hover:text-white"
+          >
+            <Ban />
+            Отмена
+          </AlertDialogCancel>
+          <AlertDialogAction
+            data-testid="confirm-delete"
+            onClick={() => {
+              dispatch(employersSlice.actions.deleteEmployer(employerId));
+              setIsOpen(false);
+            }}
+            className="direction-row flex items-center gap-2 rounded-md bg-red-500 px-3 py-2 text-sm text-white transition-colors hover:bg-red-700"
+          >
+            <Trash2 /> Удалить
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
